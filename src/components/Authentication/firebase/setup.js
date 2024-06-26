8293102cbf2d93998c63aa9787b118c71cc74363
ShaikAbdulKhaliq <shaik.a@mobiusdtaas.ai>
2024-06-26 12:12:45 +0530

    user config changed for firbase

--- src/components/Authentication/firebase/setup.js
+++ src/components/Authentication/firebase/setup.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6jRJlmW2sF4z-qkiSb0oOty5hzwpldSM",
-  authDomain: "gaianhearhere.firebaseapp.com",
-  projectId: "gaianhearhere",
-  storageBucket: "gaianhearhere.appspot.com",
-  messagingSenderId: "303151237068",
-  appId: "1:303151237068:web:333de673c2482fb4467f69",
-  measurementId: "G-33WHY4HBE8"
+  apiKey: "AIzaSyAbGtYsOEj3hbkEEk5xKrL-0LZkDKCCPDY",
+  authDomain: "hearheremobileapp.firebaseapp.com",
+  projectId: "hearheremobileapp",
+  storageBucket: "hearheremobileapp.appspot.com",
+  messagingSenderId: "356695765082",
+  appId: "1:356695765082:web:27a4603f8e0febaa54e863",
+  measurementId: "G-N4W0E2CH6Z"
 };
 
 const app = initializeApp(firebaseConfig);
